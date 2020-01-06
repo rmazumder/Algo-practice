68542bd0b50b3e144ec137d9b97633c4bacfeb94
ruhul <ruhul@Ruhul-MP-2017.local>
2020-01-05 23:49:14 -0800

    buddle sort initial

---
+++ sort/BubbleSort.java
@@ -0,0 +1,22 @@
+
+package sort;
+
+public class BubbleSort {
+	
+	
+
+	public static void main(String[] args){
+		
+
+		int array[] = new int[]{12,1,3,7,45,2214};
+
+		System.out.println("Unsorted:");
+		for (int a : array) {
+			System.out.print(a +", ") ;
+		}
+
+
+
+
+	}	
+}
