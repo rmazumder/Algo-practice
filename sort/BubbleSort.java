43febc2e2e4bb8e7022683c937e94a1d496da211
ruhul <ruhul@Ruhul-MP-2017.local>
2020-01-06 00:06:21 -0800

    buddle sort initial

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -8,14 +8,45 @@ public class BubbleSort {
 	public static void main(String[] args){
 		
 
-		int array[] = new int[]{12,1,3,7,45,2214};
+		int array[] = new int[]{12,1,3,7,45,22, 14};
 
 		System.out.println("Unsorted:");
 		for (int a : array) {
 			System.out.print(a +", ") ;
 		}
+		System.out.println();
+
+		boolean swapneeded = true;
+		boolean swappedDone = false;
+
+		while(swapneeded){
+			for (int i=0;i<array.length; i ++ ){
+				if(i != array.length - 1){
+					int a = array[i];
+					int b = array[i + 1];
+
+					if(a > b){ // needs swap
+						swappedDone = true;
+						array[i] = b;
+						array[i + 1] = a ;
+					}
+				}
+
+			}
+
+			if(!swappedDone){
+				swapneeded = false;
+			}
+			//reset swappedDone
+			swappedDone = false;
+
+		}
 
 
+		System.out.println("Sorted:");
+		for (int a : array) {
+			System.out.print(a +", ") ;
+		}
 
 
 	}	
